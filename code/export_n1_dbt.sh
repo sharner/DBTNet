ab1c1b92e937a2076f11eef4d28f9505b87a0671
Forest Rouse <forest@layerjot.com>
2022-03-12 15:58:52 -0800

    Export scripts.

---
+++ code/export_n1_dbt.sh
@@ -0,0 +1,11 @@
+python3 export_dbt.py \
+	--model resnet50 --mode hybrid \
+	--load-params ../model/train_params_default/imagenet-resnet50-999.params\
+	--num-epochs 1000 --batch-size 16 --num-gpus 0 \
+	--num-data-workers 12 \
+	--input-size 224\
+	--nclasses 457\
+	--dtype float16 \
+	--last-gamma \
+	--export-dir ../model/export/n1_training_clean_default_dbt \
+	--logging-file ../model/log/n1_export_clean_dbt.log 
