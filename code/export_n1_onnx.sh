747f5a44c0c3848a86f4960cc969915ff6b06c5a
Forest Rouse <forest@layerjot.com>
2022-04-23 11:23:19 -0700

    Minor

--- code/export_n1_onnx.sh
+++ code/export_n1_onnx.sh
@@ -1,6 +1,6 @@
 python3 export_to_onnx.py \
-	--load-params ../model/params_n1_training_clean_dbt/imagenet-resnet50-0009.params \
+	--load-params ../model/params_n1_training_clean_dbt/imagenet-resnet50-0099.params \
 	--load-syms ../model/params_n1_training_clean_dbt/imagenet-resnet50-symbol.json \
-	--onnx-file test.onnx \
+	--onnx-file dbt.224.100.onnx \
 	--input-size 224\
 	--nclasses 457
